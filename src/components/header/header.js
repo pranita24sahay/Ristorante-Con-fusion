1a4f25a74cb4c919759d8ee64f9349258038c66f
pranita24sahay <pranita24sahay@users.noreply.github.com>
2020-11-30 10:06:43 +0530

    Uncontrolled Forms

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,34 @@
 import React,{Component} from 'react'
-import {Jumbotron} from 'reactstrap'
+import { Jumbotron,Button, Modal, ModalHeader, ModalBody,Form, FormGroup, Input, Label } from 'reactstrap';
 
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+      }
+      handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }
     render() {
+        
         return(
+            <div>
+                <br />
+            
                 <Jumbotron>
                     <div className="container">
                         <div className="row row-header">
@@ -15,9 +39,31 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                        <FormGroup>
+                            <Label htmlFor="username">Username</Label>
+                            <Input type="text" id="username" name="username"innerRef={(input) => this.username = input} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="password">Password</Label>
+                            <Input type="password" id="password" name="password"innerRef={(input) => this.password = input}  />
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" name="remember"innerRef={(input) => this.remember = input}  />
+                                Remember me
+                            </Label>
+                        </FormGroup>
+                        <Button type="submit" value="submit" color="primary">Login</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
+            </div>
         );
     }
 }
-
-
 export default Header;
