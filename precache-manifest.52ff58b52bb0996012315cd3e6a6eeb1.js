a7408fdb01f2d6fff233f8468c22a7bcf0005eed
pranita24sahay <pranita24sahay@users.noreply.github.com>
2020-12-08 13:00:07 +0530

    Updates

---
+++ precache-manifest.52ff58b52bb0996012315cd3e6a6eeb1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e043562f38e614660405dcf9ac6ca317",
+    "url": "/Ristorante-Con-fusion/index.html"
+  },
+  {
+    "revision": "b8401685fb16b18ee45c",
+    "url": "/Ristorante-Con-fusion/static/css/2.25190345.chunk.css"
+  },
+  {
+    "revision": "3fc56f5fede0bdf7c2d8",
+    "url": "/Ristorante-Con-fusion/static/css/main.ec753f37.chunk.css"
+  },
+  {
+    "revision": "b8401685fb16b18ee45c",
+    "url": "/Ristorante-Con-fusion/static/js/2.e89160c2.chunk.js"
+  },
+  {
+    "revision": "1bae6c4f992c73bc9ac5e000dc7352d2",
+    "url": "/Ristorante-Con-fusion/static/js/2.e89160c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc56f5fede0bdf7c2d8",
+    "url": "/Ristorante-Con-fusion/static/js/main.c9598082.chunk.js"
+  },
+  {
+    "revision": "e43f54ae591b3bc159a0",
+    "url": "/Ristorante-Con-fusion/static/js/runtime-main.b2f53daa.js"
+  },
+  {
+    "revision": "ea8ecac0b30883a7bdeb0f57ed02dcdd",
+    "url": "/Ristorante-Con-fusion/static/media/alberto.ea8ecac0.png"
+  },
+  {
+    "revision": "96d6bd20976fae9cf5fdaccec2948966",
+    "url": "/Ristorante-Con-fusion/static/media/buffet.96d6bd20.png"
+  },
+  {
+    "revision": "a1c19f6daa4c7956e354aac00bd92ed4",
+    "url": "/Ristorante-Con-fusion/static/media/elaicheesecake.a1c19f6d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ristorante-Con-fusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ristorante-Con-fusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ristorante-Con-fusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ristorante-Con-fusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ristorante-Con-fusion/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c8fcf9ae1311406be5a7d9439b5deea5",
+    "url": "/Ristorante-Con-fusion/static/media/uthappizza.c8fcf9ae.png"
+  },
+  {
+    "revision": "986a26443f1d5bbfd4993f12ca302bd3",
+    "url": "/Ristorante-Con-fusion/static/media/vadonut.986a2644.png"
+  },
+  {
+    "revision": "e54b2833f3e00ae8ab8211cf1c0021ab",
+    "url": "/Ristorante-Con-fusion/static/media/zucchipakoda.e54b2833.png"
+  }
+]);
